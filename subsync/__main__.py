48e56921689e188626d22f712818a502032acf1a
tympanix <tympanix@gmail.com>
2018-03-30 22:04:42 +0200

    feat: added --graph flag

--- subsync/__main__.py
+++ subsync/__main__.py
@@ -2,7 +2,7 @@ import argparse
 
 def main(args):
     from media import Media
-    media = [Media(m) for m in args]
+    media = [Media(m) for m in args.media]
 
     from net import NeuralNet
     model = NeuralNet()
@@ -10,7 +10,7 @@ def main(args):
     for m in media:
         m.mfcc()
         for s in m.subtitles():
-            s.sync(model)
+            s.sync(model, plot=args.graph)
 
 
 
@@ -18,7 +18,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('media', metavar='MEDIA', type=str, nargs='+',
         help='media for which to synchronize subtitles')
+    parser.add_argument('--graph', dest="graph", action='store_true', help='show graph for subtitle synchronization')
 
     args = parser.parse_args()
 
-    main(args.media)
+    main(args)
